cd9e642c0d42fdd1c7bc525a6dc75185e99e3ff9
Romeu Gomes <oh4ianon@hotmail.com>
2017-01-21 19:24:27 +0000

    push and print function declarations

--- g_ll.h
+++ g_ll.h
@@ -1,8 +1,14 @@
 #ifndef _G_LL_H_
 #define _G_LL_H_
 
-struct gll {
+#include <stdlib.h>
 
+struct gll_node {
+    void *data;
+    struct gll_node *next;
 };
+
+void gll_push(struct gll_node *head, void *data, size_t data_size);
+void gll_print(struct gll_node *head, void (*pfnct)(void*));
 
 #endif
