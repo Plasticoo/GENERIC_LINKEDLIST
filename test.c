478d0222230bd003f42a678bc38c38288fcb2b15
Romeu Gomes <oh4ianon@hotmail.com>
2017-01-24 16:52:26 +0000

    Create example with structures.

--- test.c
+++ test.c
@@ -2,12 +2,25 @@
 
 #include <stdio.h>
 
+struct test {
+    int a;
+    int b;
+};
+
+void print_struct(void *data) {
+    struct test *c = (struct test*)data;
+    printf("Struct: %d %d\n", c->a, c->b);
+}
+
 int main(int argc, char *argv[])
 {
     struct gll_node *list = NULL;
+    struct gll_node *list_t = NULL;
 
     int i;
     int ordered[] = {1,2,3,4,5};
+
+    struct test _ordered[] = {{1,2},{3,4},{5,6}};
 
     for (i = 0; i < 5; i++) {
         gll_push_top(&list, &ordered[i], sizeof(int));
@@ -35,6 +48,19 @@ int main(int argc, char *argv[])
     gll_print(list, gll_print_byte);
 
     gll_free(list);
+
+    printf("\nWorking with complex data now...\n");
+
+    for (i = 0; i < 3; i++) {
+        gll_push_top(&list_t, &_ordered[i], sizeof(struct test));
+        gll_push_end(list_t, &_ordered[i], sizeof(struct test));
+    }
+
+    printf("Size: %d\n", gll_length(list_t));
+
+    gll_print(list_t, print_struct);
+
+    gll_free(list_t);
 
     return 0;
 }
